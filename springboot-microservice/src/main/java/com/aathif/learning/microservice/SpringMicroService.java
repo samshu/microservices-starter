22ca77dd9e24ac780701d38e3ec65e4f4f6fbfc5
aathif <aathif.md@gmail.com>
2018-10-13 21:08:20 +0530

    Added Spring Boot Web Starter

---
+++ springboot-microservice/src/main/java/com/aathif/learning/microservice/SpringMicroService.java
@@ -0,0 +1,12 @@
+package com.aathif.learning.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroService.class, args);
+	}
+}
---
+++ springboot-microservice/src/test/java/com/aathif/learning/microservice/SpringMicroServiceTests.java
@@ -0,0 +1,16 @@
+package com.aathif.learning.microservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMicroServiceTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
